caa0b5750b8182504d24c9636857deaf75a337ac
flendoh <flenditomlgcreations@gmail.com>
2024-11-20 19:05:45 -0500

    fix: responsive v1

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -2,5 +2,15 @@ $form = document.querySelector("form");
 
 $form.addEventListener("submit", (e) => {
     e.preventDefault();
-    alert("Formulario enviado");
+
+    const name = $form.elements["name"].value;
+    const email = $form.elements["email"].value;
+    const message = $form.elements["message"].value;
+
+    if(!name || !email || !message) {
+        alert("Todos los campos son obligatorios");
+        return;
+    }
+
+    alert(`Formulario enviado: ${name}, ${email}, ${message}`);
 });
